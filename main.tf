a8b4863506c313c336bb76f749059e6597f678be
muntahailmi <ilmi.muntaha@gmail.com>
2025-08-07 06:41:56 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,23 +1,7 @@
-terraform {
-  required_providers {
-    google = {
-      source = "hashicorp/google"
-      version = "6.8.0"
-    }
-  }
-}
-variable "project_id" {}
+variable "project_id" {default = ""}
 variable "region" {default = "us-central1"}
 variable "zone" {default = "us-central1-b"}
-
-provider "google" {
-  project = var.project_id
-  region  = var.region
-  zone    = var.zone
-}
-
-
-resource "google_service_account" "default" {
-  account_id   = "temp-remote-state-test"
-  display_name = "SVC to test Remote State"
+resource "local_file" "foo" {
+  content  = "example-test ${var.project_id}"
+  filename = "tes.txt"
 }
