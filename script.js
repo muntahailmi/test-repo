734d3d4ab91fd50e09855db9a2f27c17862adf55
muntahailmi <ilmi.muntaha@gmail.com>
2025-08-13 09:44:01 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var db_name = process.env.MONGODB_NAME;
 var coll_name = process.env.MONGODB_COLLECTION;
 var org_name = process.env.MONGODB_ORGNAME;
-use db_name;
+db.getSiblingDB(db_name);
 var fetch_data = db.coll_name.find({"org_id": org_name}).toArray();
 fetch_data.array.forEach(entry => {
     fs.writeFile("terraform.tfvars.json", JSON.stringify(entry.vars), err => {
