37b1de4598401af622d3174a5da6dd66805dcb9d
muntahailmi <ilmi.muntaha@gmail.com>
2025-08-15 01:50:53 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-var db_name = process.env.MONGODB_NAME;
-var coll_name = process.env.MONGODB_COLLECTION;
-var org_name = process.env.MONGODB_ORGNAME;
+var db_name = process.env.MONGO_DBNAME;
+var coll_name = process.env.MONGO_COLLECTION;
+var org_name = process.env.MONGO_ORGID;
 db.getSiblingDB(db_name);
 var fetch_data = db[coll_name].find({"org_id": org_name}).toArray();
 fetch_data.forEach(entry => {
@@ -8,7 +8,7 @@ fetch_data.forEach(entry => {
         if (err) {
             console.error("Error writing file: ", err)
         } else {
-            console.log("Data exported")
+            console.log("Data exported", JSON.stringify(entry.vars).length)
         }
     })
 });
